956b49db8d95babb7d7cbef4879d1fd1b9dcdad0
Akhuemonkhan Olugbenga <gbengaloveeyesimages@gmail.com>
2018-11-19 09:01:26 -0500

    workOUT

---
+++ python_novice101.py
@@ -0,0 +1,22 @@
+print " Hello, World! "
+print " I am Gbenga the novice CODER. "
+
+print """
+I am a cohort member
+at Digitalcraft boothcamp
+November 2018-immersive
+and my coding skills still
+sukcs after week one. """ 
+
+count = 0
+while (count < 5):
+#     print count, "is less than: 5 " 
+#     count = + 1
+# else:
+#     print "i am still a NOVICE. " # this statement crashed my and i saw it's
+                                  # my first experience with infinite loop.
+    print count, "is less than: 5 "
+    count = count + 1
+else:
+    print count, " is not less than 5 "
+    print " I am a novice. "
