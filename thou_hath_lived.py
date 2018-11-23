7a8a45d9a15fa59c734825b81a2a3c7d6cc43e06
Akhuemonkhan Olugbenga <gbengaloveeyesimages@gmail.com>
2018-11-22 23:42:22 -0500

    age in days

---
+++ thou_hath_lived.py
@@ -0,0 +1,8 @@
+def ageDays():
+    userAge = raw_input("How old hath thou, son of man?\n  ")
+    ageDays = int(userAge) * 365
+    print("thou hath lived {} days on earth" .format(ageDays))
+    
+
+ageDays()
+
