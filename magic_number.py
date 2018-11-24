2b2bb30ee1fe5ecc3a256c77f3186dc86439f63d
Akhuemonkhan Olugbenga <gbengaloveeyesimages@gmail.com>
2018-11-24 07:05:47 -0500

    guess the number

---
+++ magic_number.py
@@ -0,0 +1,16 @@
+def prog ():
+    number = "5"
+    number = raw_input("Pick a number between 1 and 10\n ")
+    if number == str(5):
+        print ("Congrats! You have won the price")
+    else:
+        print ("Try agin next time")    
+
+prog() 
+
+
+
+# 1. Create a variable that holds a "magic number" between 0 and 10
+# 2. Tell the user to pick a number between 0 and 10
+# 3. If the user pick the magic number, Tell them they have won.
+# 4. Else tell them to try again
